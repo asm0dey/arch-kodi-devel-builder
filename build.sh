57a9eef3d3211d258222a0664aef05c3dcc67f03
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-11 00:56:16 +0300

    Copy pasta fail. still had a instead of a (http://whatthecommit.com)

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true; do
+    echo Not sleeping!
+    sleep 60
+done
+
+NON_SLEEPING_PID="$!"
+
+docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -it makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm'
+
+kill $NON_SLEEPING_PID
