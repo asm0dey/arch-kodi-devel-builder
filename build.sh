e98894f4212aa6e051b4d78b1593a2c18efe6f38
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-11 09:35:11 +0300

    NOJIRA: No cry (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -3,7 +3,13 @@
 docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -i makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm' &
 NON_SLEEPING_PID="$!"
 
-(sleep 2700 && kill "$NON_SLEEPING_PID") &
+TIME_TO_RUN=40
+while true; do
+    sleep 60
+    echo Not sleeping!
+    TIME_TO_RUN=$((TIME_TO_RUN-1))
+    test $TIME_TO_RUN -eq 0 && kill $NON_SLEEPING_PID
+done &
 SLEEPING_PID="$!"
 
 wait "$NON_SLEEPING_PID" && kill "$SLEEPING_PID"
