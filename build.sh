f85db100fa0a4680bc8d02910124c42d6ad42c83
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-11 11:19:57 +0300

    s/ / /g (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -i makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm' &
 NON_SLEEPING_PID="$!"
 
-TIME_TO_RUN=35
+TIME_TO_RUN=38
 while true; do
     sleep 60
     echo Not sleeping!
     TIME_TO_RUN=$((TIME_TO_RUN-1))
-    test $TIME_TO_RUN -eq 0 && kill $NON_SLEEPING_PID
+    test $TIME_TO_RUN -eq 0 && kill -9 $NON_SLEEPING_PID
 done &
 SLEEPING_PID="$!"
 
