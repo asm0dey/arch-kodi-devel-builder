474347eaee0c647770855a9fa2b911626cd30dc9
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-11 21:41:41 +0300

    This is my code. My code is amazing. (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-DOCKER="$(docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm')"
+DOCKER="$(docker run -e "MAKEFLAGS='-j 2'" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm')"
 
 TIME_TO_RUN=41
 while true; do
@@ -11,5 +11,6 @@ while true; do
         break
     fi
     test "$TIME_TO_RUN" -eq 0 && docker stop --time 3 "$DOCKER" && break
+    test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -eq 0 && exit 1
     docker logs --tail=50 "$DOCKER"
 done
