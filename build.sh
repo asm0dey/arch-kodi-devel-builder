0b5dc9a699b347b7e95d40ff3c98324824240d82
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-11 03:21:21 +0300

    Fixing Corey's bugs. (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-while true; do
-    echo Not sleeping!
-    sleep 60
-done &
 
+docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -it makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm' &
 NON_SLEEPING_PID="$!"
-
-docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -it makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm'
-
+sleep 2700
 kill $NON_SLEEPING_PID
