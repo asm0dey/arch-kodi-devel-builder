dd26640c3bb9fc9ba19ee964024e111d31e0a2e6
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-11 03:37:55 +0300

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better. (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-
-docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -it makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm' &
+docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -i makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm' &
 NON_SLEEPING_PID="$!"
-sleep 2700
-kill $NON_SLEEPING_PID
+
+(sleep 2700 && kill "$NON_SLEEPING_PID") &
+SLEEPING_PID="$!"
+
+wait "$NON_SLEEPING_PID" && kill "$SLEEPING_PID"
