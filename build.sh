ad890f0c509d20829ec92fff9a5bb2cc108ae000
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-12 07:34:37 +0300

    added super-widget 2.0. (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 
 DOCKER="$(docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm')"
 
-ERROR=0
-TIME_TO_RUN=41
-while true; do
+TIME_TO_RUN=43
+while test "$TIME_TO_RUN" -gt 0; do
     sleep 60
     echo Not sleeping!
     TIME_TO_RUN=$((TIME_TO_RUN-1))
     if (! (docker ps -q -a --no-trunc| grep "$DOCKER" > /dev/null)); then
         break
     fi
-    test "$TIME_TO_RUN" -eq 0 && docker stop --time 3 "$DOCKER" && test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -eq 0 && break && exit 1
     docker logs --tail=50 "$DOCKER"
 done
+docker stop --time 3 "$DOCKER" && test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -eq 0  && exit 1
