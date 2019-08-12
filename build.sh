e32f6546dc98de2ee825f2004673a5cc754ca823
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-12 08:25:42 +0300

    Somebody set up us the bomb. (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-DOCKER="$(docker run -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm')"
+DOCKER="$(docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm')"
 
 TIME_TO_RUN=43
 while test "$TIME_TO_RUN" -gt 0; do
@@ -12,4 +12,8 @@ while test "$TIME_TO_RUN" -gt 0; do
     fi
     docker logs --tail=50 "$DOCKER"
 done
-docker stop --time 3 "$DOCKER" && test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -eq 0  && exit 1
+
+if (docker ps -q -a --no-trunc| grep "$DOCKER" > /dev/null); then
+    docker stop --time 3 "$DOCKER"
+fi
+test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -eq 0  && exit 0
