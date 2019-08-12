83192ce60bb794f06397916b0624a41b85531288
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-12 12:33:42 +0300

    Ryan rebase plx? (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-DOCKER="$(docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -Acs --noconfirm')"
+DOCKER="$(docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -As --noconfirm')"
 
 TIME_TO_RUN=43
 while test "$TIME_TO_RUN" -gt 0; do
@@ -11,7 +11,6 @@ while test "$TIME_TO_RUN" -gt 0; do
         break
     fi
     docker logs --tail=50 "$DOCKER"
-    (docker logs --tail=1 "$DOCKER" | grep 'Cleaning up...' > /dev/null ) &&  docker stop --time 1 "$DOCKER" && break
 done
 
 if (docker ps -q -a --no-trunc| grep "$DOCKER" > /dev/null); then
