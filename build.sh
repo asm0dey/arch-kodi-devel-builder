da9e7a2b5fa8e3bd9ab15f1c0f4e6e9922e7f896
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-15 13:03:25 +0300

    I must sleep... it's working... in just three hours... (http://whatthecommit.com)

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash -x
 
-DOCKER="$(docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -As --noconfirm')"
+timeout -k 5s 43m docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -i makepkg bash -c 'cd ~/pkg && makepkg -As --noconfirm'
 
-TIME_TO_RUN=43
-while test "$TIME_TO_RUN" -gt 0; do
-    sleep 60
-    echo Not sleeping!
-    TIME_TO_RUN=$((TIME_TO_RUN-1))
-    if (! (docker ps -q --no-trunc| grep "$DOCKER" > /dev/null)); then
-        break
-    fi
-    docker logs --tail=50 "$DOCKER"
-done
-
-if (docker ps -q -a --no-trunc| grep "$DOCKER" > /dev/null); then
-    docker stop --time 3 "$DOCKER"
-fi
-(test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -eq 0  && exit 1 )|| exit 0
+test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -gt 0  || exit 1
