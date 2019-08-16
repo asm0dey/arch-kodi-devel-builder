3c531936ff3390d72f22a25d564684f2abc5d8b1
asm0dey <pavel.finkelshtein@gmail.com>
2019-08-16 16:59:38 +0300

    a few bits tried to escape, but we caught them (http://whatthecommit.com)
    Signed-off-by: asm0dey <pavel.finkelshtein@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -x
 
-timeout -k 5s 43m docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -i makepkg bash -c 'cd ~/pkg && makepkg -As --noconfirm'
+DOCKER="$(docker run -e "MAKEFLAGS=-j2" -v "$(pwd)"/cache:/home/builder/.ccache -v "$(pwd)"/kodi:/home/builder/pkg -d makepkg bash -c 'cd ~/pkg && makepkg -As --noconfirm')"
 
-test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -gt 0  || exit 1
+TIME_TO_RUN=43
+while test "$TIME_TO_RUN" -gt 0; do
+    sleep 60
+    echo Not sleeping!
+    TIME_TO_RUN=$((TIME_TO_RUN-1))
+    if (! (docker ps -q --no-trunc| grep "$DOCKER" > /dev/null)); then
+        break
+    fi
+    docker logs --tail=50 "$DOCKER"
+done
+
+if (docker ps -q -a --no-trunc| grep "$DOCKER" > /dev/null); then
+    docker stop --time 3 "$DOCKER"
+fi
+test "$(find "$(pwd)"/kodi -name '*.tar.xz' | wc -l)" -gt 6 || exit 1
+
